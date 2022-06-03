57396cbad6f482082419871d8d6c86eb72e15c05
gumohan <3531804187@qq.com>
2022-06-03 13:10:02 +0800

    go commit

--- project1/test.go
+++ project1/test.go
@@ -9,8 +9,5 @@ func main() {
 	fmt.Println(function.Add(5, 6))
 	fmt.Println(sub(20, 5))
 	fmt.Println(sub(6, 7))
-	fmt.Println(sub(8, 9))
-	fmt.Println(sub(10, 11))
-	fmt.Println(function.Add(7, 8))
-	fmt.Println(function.Add(1, 2))
+
 }
